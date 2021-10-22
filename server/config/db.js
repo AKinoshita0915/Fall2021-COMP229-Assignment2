8465b4378c15ed6921c368fc07c512ab0d3d21ea
Akinoshita0915 <akinoshi@my.centennialcollege.ca>
2021-10-21 23:17:56 -0400

    second commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contact_store"
-    //"URI": ""
+    //"URI": "mongodb://localhost/contact_store"
+    "URI": "mongodb+srv://lecturer:QSv4GHLj2BSEFUTJ@mongodbserver.z29nl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
